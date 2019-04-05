ceadd480f04a5d65fbeb4b74145e515ac0d39957
Hiroki Yamaguchi <hiroki6357@gmail.com>
2019-04-06 00:36:33 +0900

    fix form

--- SampleCLRForm.cpp
+++ SampleCLRForm.cpp
@@ -1,2 +1,10 @@
 #include "SampleCLRForm.h"
-
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew SampleCLRForm::SampleCLRForm());
+}
